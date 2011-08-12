95883fb8dd14795c0675f30d30a75abd160c0b2c
Sivaram Omprakash <sivaram.psg@gmail.com>
2011-08-12 16:03:57 +0530

    modified the pom xml and project structure

---
+++ src/main/java/ch01/ts/TimeServerImpl.java
@@ -0,0 +1,21 @@
+package ch01.ts;
+
+import java.util.Date;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface="ch01.ts.TimeServer")
+public class TimeServerImpl implements TimeServer
+{
+
+	@Override
+	public String getTimeAsString() {
+		return new Date().toString();
+	}
+
+	@Override
+	public long getTimeAsElapsed() {
+		return new Date().getTime();
+	}
+	
+}
---
+++ src/main/java/ch01/ts/TimeServerPublisher.java
@@ -0,0 +1,12 @@
+package ch01.ts;
+
+import javax.xml.ws.Endpoint;
+
+
+public class TimeServerPublisher
+{
+	public static void main(String args[])
+	{
+		Endpoint.publish("http://127.0.0.1:9876/ts", new TimeServerImpl());
+	}
+}
